8e4ed9ce0b8b9f4e3206be96696bee5b607b113c
Idle del <magarkiran436@gmail.com>
2025-06-28 12:24:46 +0545

    Edit and Delete button functionality

--- note/urls.py
+++ note/urls.py
@@ -4,6 +4,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add_note/', views.add_note, name='add_note')
-
+    # Path to add a new note
+    path('add_note/', views.add_note, name='add_note'),
+    # Path to edit existing notes
+    path('edit_note/<int:pk>/', views.edit_note, name = 'edit_note'),
+    # Path to delete a note
+    path('delete_note/<int:pk>/', views.delete_note, name = 'delete_note')
 ]
--- note/views.py
+++ note/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import Note
 
@@ -12,3 +12,23 @@ def add_note(request):
     
         return redirect('home')
     return render(request, 'addnotes.html')
+
+def edit_note(request, pk):
+    get_note = get_object_or_404(Note, pk=pk)
+    if request.method == 'POST':
+        new_title = request.POST['title']
+        new_text = request.POST['text']
+        get_note.title = new_title
+        get_note.text = new_text
+        get_note.save()
+        return redirect('home')
+    else:
+        context = {
+            'get_note' : get_note
+        } 
+        return render(request, 'editnote.html', context)
+    # return HttpResponse('Edit Note Page')
+
+def delete_note(request, pk):
+    Note.objects.filter(pk=pk).delete()
+    return redirect('home')
