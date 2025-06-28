fca9db35abc1156f25769ea7f4999c54729bcdf1
Idle del <magarkiran436@gmail.com>
2025-06-28 11:28:32 +0545

    Add new note functionality

---
+++ note/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('add_note/', views.add_note, name='add_note')
+
+]
--- note/views.py
+++ note/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .models import Note
 
 # Create your views here.
+def add_note(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        text = request.POST['text']
+
+        Note.objects.create(title=title, text=text)
+    
+        return redirect('home')
+    return render(request, 'addnotes.html')
